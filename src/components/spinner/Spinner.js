4492e1937d7d3af8151ef83ac4b13c5575985139
DohyaOriginalNickname <tsareg.n@gmail.com>
2022-09-30 13:24:07 +0600

    create a spinner

---
+++ src/components/spinner/Spinner.js
@@ -0,0 +1,13 @@
+import './spinner.css'
+
+const spinner = () => {
+    return (
+        <div className="loadingio-spinner-rolling-emnym052qhs">
+            <div className="ldio-h2bv23z2go">
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default spinner;
