03c981f5c2b7b1b6ac33bc2d441932fe3cac1662
DohyaOriginalNickname <tsareg.n@gmail.com>
2022-10-24 16:18:11 +0600

    minor SEO-optimization

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,9 +1,17 @@
 import { Link } from "react-router-dom"
+import { Helmet } from "react-helmet"
 import ErrorMessage from "../ErrorMessage/ErrorMessage"
 
 const PageNotFound = () => {
     return (
         <div>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="404"
+                />
+                <title>Not Found</title>
+            </Helmet>
             <ErrorMessage/>
             <p style={{'color': 'red', 'textAlign': 'center', 'fontSize': '50px'}}>Чумба, ты ебнутый. Сходи к мозгоправу, попей колесики</p>
             <Link to='/' style={{"display": 'block','color': 'blue', 'textAlign': 'center', 'fontSize': '40px'}}>Нахуй из моего космического пространства</Link>
